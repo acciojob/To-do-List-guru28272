2a6e79ff555f39b9ba98bbbfb397c20f21fb8861
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-02-17 12:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+const btn = document.querySelector(`#addTodoBtn`);
 
+const ol = document.querySelector(`#todoList`);
+
+const input = document.querySelector(`#newTodoInput`);
+
+btn.addEventListener(`click`, function () {
+  if (input.value !== "") {
+    ol.innerHTML = `<li>listItem</li>
+  <li>listItem</li>
+  <li>listItem</li>
+  <li>listItem</li>`;
+  }
+});
